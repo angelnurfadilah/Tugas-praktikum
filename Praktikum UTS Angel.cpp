7de730207ecf526b556da75c68ebef99e21cf130
angelnurfadilah <angelnurfadilah1@gmail.com>
2024-10-21 20:31:02 +0700

    Update Praktikum UTS Angel.cpp

--- Praktikum UTS Angel.cpp
+++ Praktikum UTS Angel.cpp
@@ -6,7 +6,9 @@ using namespace std;
 
 int main()
 {
-	string nMahasiswa[100], jMahasiswa[100], kMahasiswa[100];
+	string nmMahasiswa[100];
+        string jlhMahasiswa[100];  
+        string kMahasiswa[100];
 	int	npm[100], bayarAwal[100], bayarCicilan[100], besaran[100], uangKuliah[100], bayar[100], terbayar[100], sisaBayar[100], i = 0;
 	char entry = 'y';
 		
@@ -15,37 +17,37 @@ int main()
 	while(entry == 'Y' || entry == 'y')
 	{
 		cout << "--------------------------------------------\n";
-		cout << "Nama Mahasiswa" << ":"; cin >> nMahasiswa[i];
+		cout << "Nama Mahasiswa" << ":"; cin >> nmMahasiswa[i];
 		cout << "NPM" << ":"; cin >> npm[i];
 		cout << "Kode" << ":"; cin >> kMahasiswa[i];
 		
 		if(kMahasiswa[i] == "SI"){
-			jMahasiswa[i] = "Sistem Informasi";
+			jlhMahasiswa[i] = "Sistem Informasi";
 			bayarAwal[i] = 2100000;
 			bayarCicilan[i] = 7;
 			besaran[i] = 300000;
 			uangKuliah[i] = bayarAwal[i] + (bayarCicilan[i] * besaran[i]);
 		}else if(kMahasiswa[i] == "TI"){
-			jMahasiswa[i] = "Teknik Informatika";
+			jlhMahasiswa[i] = "Teknik Informatika";
 			bayarAwal[i] = 2500000;
 			bayarCicilan[i] = 7;
 			besaran[i] = 300000;
 			uangKuliah[i] = bayarAwal[i] + (bayarCicilan[i] * besaran[i]);
 		}else if(kMahasiswa[i] == "KA"){
-			jMahasiswa[i] = "Komputer Akuntansi";
+			jlhMahasiswa[i] = "Komputer Akuntansi";
 			bayarAwal[i] = 1750000;
 			bayarCicilan[i] = 6;
 			besaran[i] = 200000;
 			uangKuliah[i] = bayarAwal[i] + (bayarCicilan[i] * besaran[i]);
 		}else if(kMahasiswa[i] == "MI"){
-			jMahasiswa[i] = "Manajemen Informatika";
+			jlhMahasiswa[i] = "Manajemen Informatika";
 			bayarAwal[i] = 1500000;
 			bayarCicilan[i] = 6;
 			besaran[i] = 250000;
 			uangKuliah[i] = bayarAwal[i] + (bayarCicilan[i] * besaran[i]);
 		}
 		
-		cout << "Jurusan" << ":" << jMahasiswa[i] << endl;
+		cout << "Jurusan" << ":" << jlhMahasiswa[i] << endl;
 		cout << "Pembayaran Awal" << ":" << bayarAwal[i] << endl;
 		cout << "Jumlah Cicilan" << ":" << bayarCicilan[i] << endl;
 		cout << "Besar Cicilan" << ":" << besaran[i] << endl;
